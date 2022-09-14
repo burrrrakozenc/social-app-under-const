9a3d23c16dc5efe2cb62f0b57f75defff3298871
burrrrakozenc <brk.ozenc@gmail.com>
2022-09-14 05:34:21 +0300

    react helmet added

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,6 +1,7 @@
 // import React, {useEffect} from 'react'
 import MainImage from '../assets/main.jpeg'
 import '../assets/main.css'
+import {Helmet} from "react-helmet";
 
 
 const Index = () => {
@@ -11,6 +12,11 @@ const Index = () => {
     return (
 
         <div className="main-container m-auto">
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Come Social</title>
+                <link rel="canonical" href="https://www.comesocial.app" />
+            </Helmet>
             <div className="section-1 ">
                 <div className="image-wrapper">
                     <img src={MainImage} alt=""/>
